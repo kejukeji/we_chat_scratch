d596d16b58d47afceb4d094cfdfb12825301cd50
yuzhe0918 <xiangyuzhe0918@126.com>
2014-02-26 10:58:48 +0800

    url error update

--- scratch/urls.py
+++ scratch/urls.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from scratch import app
-from flask.ext import restful
+
 # 微信页面
 from .views.web import scratch_home
 from .views.test import do_login
@@ -12,17 +12,8 @@ from scratch.views.sjm_login import register,register_action,showusers
 from scratch.restful.userlist import UserRestful,UserByNameRestful
 app.add_url_rule('/scratch', 'weixin', weixin, methods=('GET', 'POST'))
 app.add_url_rule('/scratch/home', 'scratch_home', scratch_home, methods=('GET', 'POST'))
-<<<<<<< HEAD
-app.add_url_rule('/login', 'to_login', do_login, methods=('GET', 'POST'))
-=======
 app.add_url_rule('/login','login_tohome', login_tohome,methods=('GET','POST'))
 app.add_url_rule('/loginto','login_in',login_in,methods=('GET','POST'))
 app.add_url_rule('/register','register',register,methods=('GET','POST'))
 app.add_url_rule('/registeraction','register_action',register_action,methods=('GET','POST'))
 app.add_url_rule('/show','showusers',showusers,methods=('GET','POST'))
-
-
-api = restful.Api(app)
-api.add_resource(UserRestful, '/restful/user')
-api.add_resource(UserByNameRestful,'/restful/name')
->>>>>>> 9363138e29c58cb69f27cbe57b4c1625f89a4823
