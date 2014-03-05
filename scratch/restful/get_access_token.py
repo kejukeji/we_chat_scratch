d80beceed1d4dea84032f7c5c6bfe8929f9735a7
yuzhe0918 <xiangyuzhe0918@126.com>
2014-03-05 15:19:44 +0800

    重构微信授权。

--- scratch/restful/get_access_token.py
+++ scratch/restful/get_access_token.py
@@ -1,5 +1,6 @@
 # coding: UTF-8
-from flask import request
+from flask import request, render_template
+from scratch.weixin.webchat import *
 import urllib2
 from urllib import urlencode
 import json
@@ -9,24 +10,26 @@ reload(sys)
 def get_token():
     '''获取token'''
     code = request.args.get('code') # 得到用户同意授权的code
-    get_access_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd66b61798e08ff5e&secret=d04dd7c764cc9b348d390eb81340a778&code='+ code +'&grant_type=authorization_code'
-    access_token = urllib2.urlopen(get_access_token_url) # 访问此url得到token
-    json_token = access_token.read() # 读取token
-
-    token = json.loads(json_token)['access_token'] # 从读取到的json中通过key得到access_token值
-    open_id = json.loads(json_token)['openid'] # 获得用户的openId
-
-    get_user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='+ token +'&openid='+ open_id +'&lang=zh_CN'
-
-    user_info_result = urllib2.urlopen(get_user_info_url) # 请求此url的结果
-    json_user_info = user_info_result.read() # 得到json结果
-
-    nickname = json.loads(json_user_info)['nickname'] # 得到用户的昵称
-    return """
-    <html>
-    <head><title>用户昵称</title></head>
-    <body>
-    <input type='text' value='"""+str(access_token)+"""'/>
-    </body>
-    </html>
-    """
+    appid = 'wx55970915710ceae8'
+    secret = '0a9fcd79087745628d8eb5dd5fb9c418'
+    weChat = WebChat('1234',appid, secret, code)
+    result = weChat.oauth_user_info() # 得到授权后用户信息json字符串
+    nickname = json.loads(result)['nickname']
+    if result != 'None':
+        return """
+        <html>
+        <head><title>Error</title></head>
+        <body>
+            <h1>"""+ nickname +"""</h1>
+        </body>
+        </html>
+        """
+    else:
+        return """
+        <html>
+        <head><title>Error</title></head>
+        <body>
+            <h1>Error Internal system error</h1>
+        </body>
+        </html>
+        """
