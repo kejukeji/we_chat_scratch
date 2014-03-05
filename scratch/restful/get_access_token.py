8f36baaa880276196be3966e0e148d3667a84ab7
yuzhe0918 <xiangyuzhe0918@126.com>
2014-03-05 15:27:52 +0800

    opened,secret

--- scratch/restful/get_access_token.py
+++ scratch/restful/get_access_token.py
@@ -10,9 +10,9 @@ reload(sys)
 def get_token():
     '''获取token'''
     code = request.args.get('code') # 得到用户同意授权的code
-    appid = 'wx55970915710ceae8'
-    secret = '0a9fcd79087745628d8eb5dd5fb9c418'
-    weChat = WebChat('1234',appid, secret, code)
+    appid = 'wxd66b61798e08ff5e'
+    secret = 'd04dd7c764cc9b348d390eb81340a778'
+    weChat = WebChat('7072',appid, secret, code)
     result = weChat.oauth_user_info() # 得到授权后用户信息json字符串
     nickname = json.loads(result)['nickname']
     if result != 'None':
